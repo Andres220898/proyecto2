77e00443b557de9c3c9ff0ad177bf0aecc396b8c
diego andres joven velasquez <andresv190216@gmail.com>
2023-09-18 19:33:50 -0500

    actualizacion 4 trimestre/andres

--- ingresoU.php
+++ ingresoU.php
@@ -17,28 +17,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Obtiene el correo electrónico y la contraseña del formulario
-    $correo = mysqli_real_escape_string ($conn, $_POST["correo"]);
+    $correo = mysqli_real_escape_string($conn, $_POST["correo"]);
     $contraseña = $_POST["contraseña"];
 
     // Construye la consulta SQL para buscar el usuario en la tabla "persona"
     $sql = "SELECT * FROM persona WHERE correo = '$correo'";
- 
+
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         // Verifica si la contraseña ingresada coincide con la contraseña almacenada en la base de datos
-        echo "Contraseña ingresada en el formulario: " . $contraseña; // Depuración
-        echo "Contraseña almacenada en la base de datos: " . $row["contraseña"]; // Depuración
-        if (password_verify($contraseña, $row["contraseña"])) {
+        if ($contraseña == $row["contraseña"]) {
             // La contraseña es válida, por lo que el inicio de sesión es exitoso
             session_start();
             $_SESSION["persona"] = $correo; // Almacena el correo electrónico del usuario en la sesión
-            header("Location: dashboard.html"); // Redirige al usuario a la página de inicio después del inicio de sesión exitoso
-            exit();
+
+            // Redirige según el usuario y el rol
+            if ($correo == 'diego@gmail.com') {
+                header("Location: dashboard.html");
+                exit();
+            } else {
+                header("Location: catalogo.html");
+                exit();
+            }
         } else {
             // Contraseña incorrecta
-            echo "<script>alert('Contraseña incorrecta');</script>";
+            echo "<script>alert('Contraseña incorrecta');location.assign('ingreso.html')</script>";
         }
     } else {
         // Usuario no encontrado
